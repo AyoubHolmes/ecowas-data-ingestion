a0c20bb2bc6a9f5459f274773313aceaa9bad07d
zaradacht <zerdest.taifour@gmail.com>
2023-07-26 12:17:33 +0100

    added database logic

--- data_backend/package/__init__.py
+++ data_backend/package/__init__.py
@@ -8,19 +8,19 @@ class Package:
     def __init__(self, config):
         self.config = config
         self.model = None
+        self.engine = None
 
     def create_connection(self):
         if self.config is None:
             raise ValueError("Configuration is not initialized. Call init_config() first.")
 
-        self.model = self.get_dbs_model(self.config.DATABASE_SYSTEM)
-        self.model = self.model(self.config)
-        self.model.connect()
+        self.model = self._get_dbs_model(self.config)
+        self.engine = self.model.connect()
 
-    def get_dbs_model(self, database_system):
-        if database_system == "postgres":
-            from data_backend.package.model_postgres import PostgresModel
-            model = PostgresModel
+    def _get_dbs_model(self, config):
+        if config.DATABASE_SYSTEM == "postgres":
+            from package.model_postgres import PostgresModel
+            model = PostgresModel(config)
         else:
             raise ValueError(
                 "No appropriate database model found. Please check your database_system in config.py"
--- data_backend/package/model_postgres.py
+++ data_backend/package/model_postgres.py
@@ -1,7 +1,6 @@
-import psycopg2
-from psycopg2 import OperationalError
 from data_backend.custom_logger import CustomLogger
-
+from sqlalchemy import create_engine, inspect
+from data_backend.package.table_definitions import Base, Country, Kpi, KpiValue
 
 logging = CustomLogger("POSTGRES MODEL")
 
@@ -9,50 +8,46 @@ logging = CustomLogger("POSTGRES MODEL")
 class PostgresModel:
     def __init__(self, config):
         self.config = config
-        self.connection = None
+        self.engine = None
+        self.table_classes = [Country, Kpi, KpiValue]
 
     def connect(self):
+        db_connection_string = (
+            f"postgresql://{self.config.POSTGRES_USER}:{self.config.POSTGRES_PASSWORD}"
+            f"@{self.config.POSTGRES_HOST}:{self.config.POSTGRES_PORT}/{self.config.POSTGRES_DB}"
+        )
         try:
-            # Connect to the default postgres database to check if the database exists
-            self.connection = psycopg2.connect(
-                database="postgres",
-                user=self.config.POSTGRES_USER,
-                password=self.config.POSTGRES_PASSWORD,
-                host=self.config.POSTGRES_HOST,
-                port=self.config.POSTGRES_PORT,
-            )
-            self.connection.autocommit = True
-            self._create_db_if_not_exists()
-            self.connection.close()
-            # Reconnect to the database
-            self.connection = psycopg2.connect(
-                database=self.config.POSTGRES_DB,
-                user=self.config.POSTGRES_USER,
-                password=self.config.POSTGRES_PASSWORD,
-                host=self.config.POSTGRES_HOST,
-                port=self.config.POSTGRES_PORT,
-            )
-            self.connection.autocommit = True
+            self.engine = create_engine(db_connection_string)
+            connection = self.engine.connect()
+            connection.close()
             logging.info("Connected to the Database Successfully! :)")
-        except OperationalError as e:
+            return self.engine
+        except Exception as e:
             logging.error(
                 f"An error occurred while connecting to the database: {str(e)}"
             )
+            logging.error("Exiting...")
+            exit(1)
 
-    
-    def _create_db_if_not_exists(self):
-        cursor = self.connection.cursor()
-        cursor.execute(f"SELECT 1 FROM pg_catalog.pg_database WHERE datname = '{self.config.POSTGRES_DB}';")
-        exists = cursor.fetchone()
-        # Create the database if it doesn't exist
-        if not exists:
-            cursor.execute(f"CREATE DATABASE {self.config.POSTGRES_DB};")
-            logging.info(f"Created database {self.config.POSTGRES_DB}")
-        else:
-            logging.info(f"Database {self.config.POSTGRES_DB} already exists")
-        cursor.close()
+    def create_tables_if_not_exists(self):
+        try:
+            if not all(self._table_exists(t) for t in self.table_classes):
+                Base.metadata.create_all(self.engine, checkfirst=True)
+                logging.info("Created tables successfully! :)")
+            else:
+                logging.info("Tables already exist. Moving on...")
+        except Exception as e:
+            logging.error(f"An error occurred while creating tables: {str(e)}")
 
+    def _table_exists(self, table_cls):
+        inspector = inspect(self.engine)
+        return inspector.has_table(table_cls.__tablename__)
     
-
-    
-    
+    def load_csv_to_pg(self, csv, table_name, if_exists="fail", index=False):
+        try:
+            csv.to_sql(table_name, self.engine, if_exists=if_exists, index=index)
+            logging.info(f"Table {table_name} loaded successfully!")
+        except Exception as e:
+            logging.error(f"Failed to load table {table_name}: {e}")
+            logging.error("Exiting...")
+            exit(1)

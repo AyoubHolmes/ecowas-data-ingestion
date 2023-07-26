5f615a0adb673c07835272942cbd7282143488cc
zaradacht <zerdest.taifour@gmail.com>
2023-07-26 11:18:03 +0100

    created a separate directory for the data back-end project

--- data-backend/main.py
+++ data-backend/main.py
@@ -1,13 +1,15 @@
 from package import Package
 from config import ConfigParser
 
+
 def main():
     # Initialize the config
     config = ConfigParser()
-    
+
     # Initialize the EcoWAS
     ecowas = Package(config)
     ecowas.create_connection()
+
 
 if __name__ == "__main__":
     main()

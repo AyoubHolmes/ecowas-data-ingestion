49ec29788e306087aee48e7599126a43f10a3f7e
zaradacht <zerdest.taifour@gmail.com>
2023-07-27 15:44:18 +0100

    added data processing main class

--- data_backend/data_processing/__init__.py
+++ data_backend/data_processing/__init__.py
@@ -1,23 +1,52 @@
-import pandas as pd
+from sqlalchemy.ext.declarative import declarative_base
+
+from data_backend.custom_logger import CustomLogger
+
+logging = CustomLogger("DataProcessor")
 
 
 class DataProcessor:
-    def __init__(self, file_path, model):
-        self.file_path = file_path
-        self.file_extension = self.get_file_extension()
-        self.df = None
-        self.model = model
 
-    def get_file_extension(self):
-        return self.file_path.split(".")[-1]
+    def __init__(self, sources, table_definitions: declarative_base()):
+        self.sources = sources
+        self.table_definitions = table_definitions
 
-    def load_file(self):
-        if self.file_extension == "csv":
-            self.df = pd.read_csv(self.file_path)
-        elif self.file_extension == "xlsx":
-            self.df = pd.read_excel(self.file_path)
-        else:
-            raise ValueError("File extension not supported")
+    def load_dfs(self):
+        logging.info("Loading dataframes...")
+        for source in self.sources:
+            self.sources[self.sources.index(source)]['df'] = source['model'].df()
 
-    def add_timestamp(self):
-        self.df['date'] = pd.Timestamp.now().strftime('%Y-%m-%d')
+    def validate_destination_tables(self):
+        logging.info("Validating destination tables...")
+        for source in self.sources:
+            if not self._table_exists(source['destination_table']):
+                raise ValueError(
+                    f"Table {source['destination_table']} does not exist! Please check your data_sources.yml file.")
+
+    def validate_source_columns(self):
+        logging.info("Validating source columns...")
+        for source in self.sources:
+            logging.info(f"Validating source columns for {source['name']}...")
+            # Get the header columns
+            columns = source['df'].columns.tolist()
+            logging.info(f"Columns in File {source['name']}: {columns}")
+            # Get the destination table columns
+            table_columns = self.table_definitions.metadata.tables[source['destination_table']].columns.keys()
+            logging.info(f"Columns in Table {source['destination_table']}: {table_columns}")
+            # Check if all the columns in the source file are in the destination table
+            if not all(elem in table_columns for elem in columns):
+                raise ValueError(
+                    f"Columns in {source['name']} do not match the destination table {source['destination_table']}")
+
+    # Check if a table name exists in Base.metadata
+    def _table_exists(self, table_name):
+        return table_name in self.table_definitions.metadata.tables
+
+    def process_source_column_names(self, skip: list):
+        # keep only the tables that are not in the skip list
+        sources = [source for source in self.sources if source['name'] not in skip]
+        logging.info(
+            f"Processing source column names for the source names {[source['name'] for source in sources]}...")
+        for source in sources:
+            # replace df column names with the destination table column names using source['column_mapping']
+            self.sources[self.sources.index(source)]['df'].rename(columns=source['column_mapping'], inplace=True)

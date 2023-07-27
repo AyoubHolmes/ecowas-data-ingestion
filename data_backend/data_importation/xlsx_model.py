7ca041fc1d05f6b58491477e88f289638b24fc72
zaradacht <zerdest.taifour@gmail.com>
2023-07-27 18:37:21 +0100

    fixed values in data

--- data_backend/data_importation/xlsx_model.py
+++ data_backend/data_importation/xlsx_model.py
@@ -9,6 +9,6 @@ class XlsxModel:
         self.HEADER = config["HEADER"]
         self.USECOLS = config["USECOLS"]
 
-    def df(self, nrows=None):
+    def df(self):
         return pd.read_excel(io=self.PATH, sheet_name=self.SHEET_NAME, header=self.HEADER,
-                             usecols=self.USECOLS, engine='openpyxl', nrows=nrows)
+                             usecols=self.USECOLS, engine='openpyxl')
